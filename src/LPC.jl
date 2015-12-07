af691db92e957bec4a3b89c9bf5f278a1503559f
Kobi Kai Calev <kcalev@gmail.com>
2015-12-07 17:05:43 +0200

    error variance est, depends on p on on signal len

--- src/LPC.jl
+++ src/LPC.jl
@@ -9,9 +9,10 @@ module LPC
 # [ ] lpc-acorr
 # [ ] lpc-covar
 # [ ] Line-Spectral-Pairs
+# [ ] power-spectrum est
 #
 
-export lpc_burg
+export lpc_burg, pow_spect
 
 function lpc_burg{T_NUM <: Number}( x::Vector{T_NUM}, p::Int)
 # LPC (Linear-Predictive-Code) estimation, using the Burg-method
@@ -44,14 +45,18 @@ function lpc_burg{T_NUM <: Number}( x::Vector{T_NUM}, p::Int)
 # (c) Kobic, 2009, 2015, MIT License
 # Translated my old(er) matlab code into julia::
 
+# TODO: check memory allocations, gc etc!
+
     ef = x    # forward error
     eb = x    # backwards error
     a = zeros(p+1)  # prediction coeffs
-    a[1] = one(T_NUM)
+    a[1] = 1
+#    sizehint!(a, p+1) # ?
     refl = zeros(T_NUM, p) # reflection coeffs
     k = 0.  
 
-    prediction_err_ = x'*x ./ length(x)
+#    prediction_err_ = x'*x ./ length(x)
+    prediction_err_ = x'*x ./ p
     prediction_err = prediction_err_[1]
 
     for m in 1:p
@@ -62,12 +67,25 @@ function lpc_burg{T_NUM <: Number}( x::Vector{T_NUM}, p::Int)
         refl[m] = k           # save reflection coeffs (if needed)
         ef = efp + k.*ebp
         eb = ebp + k.*efp
-        a = [a[1:m], zero(T_NUM)] + k.*[zero(T_NUM), a[m:-1:1]]
-        prediction_err .*= (one(T_NUM) - k*k)
+#        a = [a[1:m], zero(T_NUM)] + k.*[zero(T_NUM), a[m:-1:1]]
+        # ^ try with constant size arrays instead...
+        a[1:m+1] = [a[1:m], 0] + k.*[0, a[m:-1:1]]
+#        prediction_err .*= (one(T_NUM) - k*k)
+        prediction_err .*= (1 - k*k)
     end
 
     return a, prediction_err, refl 
 end
+
+
+
+function pow_spect(ar, err, dft_order = 256)
+    f = fft( [ar, zeros(dft_order - length(ar))] )
+    psd = err ./ (abs(f).^2 + eps() )
+
+    return psd
+end
+
 
 end # module
 
